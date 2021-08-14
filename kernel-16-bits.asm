243911a41b278dda9aad8d7beb99f5f29ce60f88
atolmaker <atol@localhost.localdomain>
2021-08-14 10:41:49 -0400

    Added an update screen function.

--- kernel-16-bits.asm
+++ kernel-16-bits.asm
@@ -1,10 +1,10 @@
 	[bits 16]
+
 ;
 ;
 ; 7c0h arrival section:
 ;
 ;
-
 mov ax, cs
 mov ds, ax
 mov es, ax
@@ -16,8 +16,9 @@ jmp enable_a20
 	%include "video-16-bits.asm"
 	%include "vga-16-bits.asm"
 	%include "gdt-16-bits.asm"
-	arrived:
+arrived:
 db 0, "Kernel found at location 0x10*10*10.", 11111111b
+
 ;
 ;
 ; Mode section:
@@ -34,7 +35,6 @@ mov eax, cr0
 or eax, 1
 mov cr0, eax
 jmp code_segment:pmode_32
-	
 	[bits 32]
 pmode_32:
 mov ax, data_segment
@@ -45,22 +45,25 @@ mov gs, ax
 mov ss, ax
 mov ebp, 0x90000
 mov esp, ebp
-operating_system:
+kernel:
 mov byte [0x000b8000], 'K'
 mov byte [0x000b8002], 'e'
 mov byte [0x000b8004], 'r'
 mov byte [0x000b8006], 'n'
 mov byte [0x000b8008], 'e'
 mov byte [0x000b800a], 'l'
-cli
-hlt
+
 ;
 ;
 ; Kernel-OS section
 ;
 ;
-	[bits 32]
 section .text
+	extern os
+	call os
+mov byte [0x000b8000], 'T'
+cli
+hlt
 section .bss
 section .rodata
 section .data
--- os.c
+++ os.c
@@ -1,34 +1,79 @@
 #include "compile-targets.h"
 
-typedef unsigned char Byte;	// 1 byte (8 bits)
+typedef unsigned char  Byte;	// 1 byte (8 bits)
 typedef unsigned short Short;	// 2 bytes (16 bits)
-typedef unsigned long Long;	// 4 bytes (32 bits)
+typedef unsigned long  Long;	// 4 bytes (32 bits)
+
+static const Short vgaWidth = 80;
+static const Short vgaHeight = 25;
 
 enum vgacolor
 {
-	vgablack=0,
-	vgablue=1,
-	vgagreen=2,
-	vgacyan=3,
-	vgared=4,
+	black = 0,
+	blue = 1,
+	green = 2,
+	cyan = 3,
+	red = 4,
 };
 
-Long os(){
+static inline Byte vgatheme
+(enum vgacolor fg, enum vgacolor bg)
+{
+	return fg | bg << 4;
+}
 
+static inline Short charoffset
+(Byte character, Byte color)
+{
+	return (Short) character | (Short)color << 8;
+}
+
+// Void to convert?
+Long strlen(const char* str)
+{
+	Long len = 0;
+	while (str[len]) len++;
+	return len;
+}
+
+void updatescreen()
+{
+	Long row = 0;
+	Long col = 0;
+	Byte pallete = vgatheme(cyan, blue);
+	Short* buffer = (Short*) 0xb8000;
+for (Long y = 0; y < vgaHeight; y++)
+{
+	for (Long x = 0; x < vgaWidth; x++)
+	{
+		const Long index = y * vgaWidth + x;
+		buffer[index] = charoffset(' ', pallete);
+	}
+} }
+
+Long os()
+{
 	Long status = 1;
 
-do{
+	do {
 
-if (status == 0) {
+if (status == 0)
+{
 	__asm__("cli \n"
-		"hlt \n"); }
+		"hlt \n");
+}
 
-else if (status == 1) {
-	status = 0; }
+else if (status == 1)
+{
+	updatescreen();
+	status = 0;
+}
 
-else {
-	return status; } }
-
-	while (status != 10);
-
-return 10; }
+else 
+{
+	return status;
+}
+	
+} while (status != 10);
+return 10;
+}
